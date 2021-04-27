08ef496d50e96b4fed1dfed250e4c98a2d8cbfd6
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-04-27 20:18:28 +0200

    Use Massive Catalogs SNAPSHOT version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,12 @@ repositories {
     jcenter()
     google()
     gradlePluginPortal()
+    maven {
+        setUrl("https://oss.sonatype.org/content/repositories/snapshots")
+        content {
+            includeGroup("com.javiersc.massive-catalogs")
+        }
+    }
 }
 
 dependencies {
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -13,6 +13,12 @@ val massiveCatalogs: String = properties.getProperty("massiveCatalogs")
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        maven {
+            setUrl("https://oss.sonatype.org/content/repositories/snapshots")
+            content {
+                includeGroup("com.javiersc.massive-catalogs")
+            }
+        }
     }
 
     versionCatalogs {

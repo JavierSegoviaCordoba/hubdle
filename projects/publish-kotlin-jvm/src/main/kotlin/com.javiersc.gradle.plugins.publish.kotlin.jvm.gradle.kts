3799beba6e67fca2563a104e83b2be813487d11e
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-10-10 14:47:34 +0200

    Improve `publish` plugins config

--- projects/publish-kotlin-jvm/src/main/kotlin/com.javiersc.gradle.plugins.publish.kotlin.jvm.gradle.kts
+++ projects/publish-kotlin-jvm/src/main/kotlin/com.javiersc.gradle.plugins.publish.kotlin.jvm.gradle.kts
@@ -1,4 +1,5 @@
 import com.javiersc.plugins.core.isSignificant
+import com.javiersc.plugins.publishing.core.configurePublishing
 import com.javiersc.plugins.publishing.core.signPublications
 
 plugins {
@@ -20,43 +21,10 @@ val sourcesJar by project.tasks.creating(Jar::class) {
     from((project.properties["sourceSets"] as SourceSetContainer)["main"].allSource)
 }
 
-configure<PublishingExtension> {
-    publications {
-        withType<MavenPublication> {
-            pom {
-                name.set(property("pom.name").toString())
-                description.set(property("pom.description").toString())
-                url.set(property("pom.url").toString())
-
-                licenses {
-                    license {
-                        name.set(property("pom.license.name").toString())
-                        url.set(property("pom.license.url").toString())
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set(property("pom.developer.id").toString())
-                        name.set(property("pom.developer.name").toString())
-                        email.set(property("pom.developer.email").toString())
-                    }
-                }
-
-                scm {
-                    url.set(property("pom.smc.url").toString())
-                    connection.set(property("pom.smc.connection").toString())
-                    developerConnection.set(property("pom.smc.developerConnection").toString())
-                }
-            }
-
-            artifact(docsJar)
-
-            artifact(sourcesJar)
-        }
-        create<MavenPublication>("maven") { from(components["java"]) }
-    }
-}
+configurePublishing(
+    artifacts = listOf(docsJar, sourcesJar),
+    components = mapOf("maven" to "java"),
+)
 
 configure(SigningExtension::signPublications)
 
--- projects/publishing-core/src/main/kotlin/PublishingExtensions.kt
+++ projects/publishing-core/src/main/kotlin/PublishingExtensions.kt
@@ -1,89 +1,96 @@
 package com.javiersc.plugins.publishing.core
 
-import com.javiersc.plugins.core.isSignificant
+import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.kotlin.dsl.getByName
-import org.gradle.plugins.signing.SigningExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.jvm.tasks.Jar
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.withType
 
-fun SigningExtension.signPublications() {
-    if (isSnapshot.not() && project.isSignificant) {
-        try {
-            signInMemory()
-        } catch (_: Throwable) {
-            useGpgCmd()
-        }
-        sign(project.extensions.getByName<PublishingExtension>("publishing").publications)
-    }
-}
+fun Project.configurePublishing(
+    artifacts: List<Jar>,
+    components: Map<String, String> = emptyMap(),
+) {
+    val publishingExtension: PublishingExtension? = extensions.findByType()
 
-fun SigningExtension.signInMemory() {
-    if (hasSigningKeyIdGradleProperty || hasSigningKeyIdEnvironmentVariable) {
-        useInMemoryPgpKeys(signingKeyId, signingKey, signingKeyPassphrase)
+    if (publishingExtension == null) {
+        errorMessage("`maven-publish` plugin is not being applied")
     } else {
-        useInMemoryPgpKeys(signingKey, signingKeyPassphrase)
-    }
-}
+        configure<PublishingExtension> {
+            publications { publicationContainer ->
+                for ((name, component) in components) {
+                    publicationContainer.create<MavenPublication>(name) {
+                        from(this@configurePublishing.components[component])
+                    }
+                }
 
-val SigningExtension.isSnapshot: Boolean
-    get() = project.version.toString().endsWith("-SNAPSHOT")
+                publicationContainer.withType<MavenPublication> {
+                    artifacts.forEach(::artifact)
 
-val SigningExtension.signingKeyName: String?
-    get() = getSigningVariable("signing.gnupg.keyName", "SIGNING_KEY_NAME")
+                    pom { pom ->
+                        pom.name.set(pomName)
+                        pom.description.set(pomDescription)
+                        pom.url.set(pomUrl)
 
-val SigningExtension.hasSigningKeyNameGradleProperty: Boolean
-    get() = project.properties["signing.gnupg.keyName"]?.toString().isNullOrBlank().not()
+                        pom.licenses { licenses ->
+                            licenses.license { license ->
+                                license.name.set(pomLicenseName)
+                                license.name.set(pomLicenseUrl)
+                            }
+                        }
 
-val SigningExtension.hasSigningKeyNameEnvironmentVariable: Boolean
-    get() = System.getenv("SIGNING_KEY_NAME").isNullOrBlank().not()
+                        pom.developers { developers ->
+                            developers.developer { developer ->
+                                developer.id.set(pomDeveloperId)
+                                developer.name.set(pomDeveloperName)
+                                developer.email.set(pomDeveloperEmail)
+                            }
+                        }
 
-val SigningExtension.signingKeyId: String?
-    get() = getSigningVariable("signing.gnupg.keyId", "SIGNING_KEY_ID")
-
-val SigningExtension.hasSigningKeyIdGradleProperty: Boolean
-    get() = project.properties["signing.gnupg.keyId"]?.toString().isNullOrBlank().not()
-
-val SigningExtension.hasSigningKeyIdEnvironmentVariable: Boolean
-    get() = System.getenv("SIGNING_KEY_ID").isNullOrBlank().not()
-
-val SigningExtension.signingKeyPassphrase: String?
-    get() = getSigningVariable("signing.gnupg.passphrase", "SIGNING_KEY_PASSPHRASE")
-
-val SigningExtension.hasSigningKeyPassphraseGradleProperty: Boolean
-    get() = project.properties["signing.gnupg.passphrase"]?.toString().isNullOrBlank().not()
-
-val SigningExtension.hasSigningKeyPassphraseEnvironmentVariable: Boolean
-    get() = System.getenv("SIGNING_KEY_PASSPHRASE").isNullOrBlank().not()
-
-val SigningExtension.signingKey: String?
-    get() = getSigningVariable("signing.gnupg.key", "SIGNING_KEY")
-
-val SigningExtension.hasSigningKeyGradleProperty: Boolean
-    get() = project.properties["signing.gnupg.key"]?.toString().isNullOrBlank().not()
-
-val SigningExtension.hasSigningKeyEnvironmentVariable: Boolean
-    get() = System.getenv("SIGNING_KEY").isNullOrBlank().not()
-
-private const val RESET = "\u001B[0m"
-private const val YELLOW = "\u001B[0;33m"
-
-private fun SigningExtension.errorMessage(message: String) =
-    project.logger.lifecycle("${YELLOW}$message$RESET")
-
-private fun SigningExtension.getSigningVariable(
-    propertyName: String,
-    environmentVariableName: String
-): String? {
-    val property: String? = project.properties[propertyName]?.toString()
-    val environmentVariable: String? = System.getenv(environmentVariableName)
-
-    when {
-        property.isNullOrBlank() && environmentVariable.isNullOrBlank() -> {
-            errorMessage(
-                "$propertyName Gradle property and " +
-                    "$environmentVariableName environment variable are missing",
-            )
+                        pom.scm { smc ->
+                            smc.url.set(pomSmcUrl)
+                            smc.connection.set(pomSmcConnection)
+                            smc.developerConnection.set(pomSmcDeveloperConnection)
+                        }
+                    }
+                }
+            }
         }
     }
-
-    return property ?: environmentVariable
 }
+
+val Project.pomName: String?
+    get() = getVariable("pom.name", "POM_NAME")
+
+val Project.pomDescription: String?
+    get() = getVariable("pom.description", "POM_DESCRIPTION")
+
+val Project.pomUrl: String?
+    get() = getVariable("pom.url", "POM_URL")
+
+val Project.pomLicenseName: String?
+    get() = getVariable("pom.license.name", "POM_LICENSE_NAME")
+
+val Project.pomLicenseUrl: String?
+    get() = getVariable("pom.license.url", "POM_LICENSE_URL")
+
+val Project.pomDeveloperId: String?
+    get() = getVariable("pom.developer.id", "POM_DEVELOPER_ID")
+
+val Project.pomDeveloperName: String?
+    get() = getVariable("pom.developer.name", "POM_DEVELOPER_NAME")
+
+val Project.pomDeveloperEmail: String?
+    get() = getVariable("pom.developer.email", "POM_DEVELOPER_EMAIL")
+
+val Project.pomSmcUrl: String?
+    get() = getVariable("pom.smc.url", "POM_SMC_URL")
+
+val Project.pomSmcConnection: String?
+    get() = getVariable("pom.smc.connection", "POM_SMC_CONNECTION")
+
+val Project.pomSmcDeveloperConnection: String?
+    get() = getVariable("pom.smc.developerConnection", "POM_SMC_DEVELOPER_CONNECTION")

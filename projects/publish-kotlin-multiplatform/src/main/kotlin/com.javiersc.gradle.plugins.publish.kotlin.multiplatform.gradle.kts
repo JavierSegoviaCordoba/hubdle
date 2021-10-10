73efaf11784c798c0dd85e71a7a5dd2eae36a2d5
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-10-10 14:47:34 +0200

    Improve `publish` plugins config

--- projects/publish-kotlin-multiplatform/src/main/kotlin/com.javiersc.gradle.plugins.publish.kotlin.multiplatform.gradle.kts
+++ projects/publish-kotlin-multiplatform/src/main/kotlin/com.javiersc.gradle.plugins.publish.kotlin.multiplatform.gradle.kts
@@ -1,4 +1,5 @@
 import com.javiersc.plugins.core.isSignificant
+import com.javiersc.plugins.publishing.core.configurePublishing
 import com.javiersc.plugins.publishing.core.signPublications
 
 plugins {
@@ -13,40 +14,7 @@ val docsJar by project.tasks.creating(Jar::class) {
     archiveClassifier.set("javadoc")
 }
 
-configure<PublishingExtension> {
-    publications {
-        withType<MavenPublication> {
-            pom {
-                name.set(property("pom.name").toString())
-                description.set(property("pom.description").toString())
-                url.set(property("pom.url").toString())
-
-                licenses {
-                    license {
-                        name.set(property("pom.license.name").toString())
-                        url.set(property("pom.license.url").toString())
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set(property("pom.developer.id").toString())
-                        name.set(property("pom.developer.name").toString())
-                        email.set(property("pom.developer.email").toString())
-                    }
-                }
-
-                scm {
-                    url.set(property("pom.smc.url").toString())
-                    connection.set(property("pom.smc.connection").toString())
-                    developerConnection.set(property("pom.smc.developerConnection").toString())
-                }
-            }
-
-            artifact(docsJar)
-        }
-    }
-}
+configurePublishing(artifacts = listOf(docsJar))
 
 configure(SigningExtension::signPublications)
 

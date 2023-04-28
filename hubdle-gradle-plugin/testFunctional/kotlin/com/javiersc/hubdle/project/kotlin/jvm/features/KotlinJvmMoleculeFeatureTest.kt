35c7c542e853b5c54b5c17e0591dfdebb4d56de6
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-04-28 18:15:58 +0200

    Ignore Kotlin JVM molecule test on Windows

--- hubdle-gradle-plugin/testFunctional/kotlin/com/javiersc/hubdle/project/kotlin/jvm/features/KotlinJvmMoleculeFeatureTest.kt
+++ hubdle-gradle-plugin/testFunctional/kotlin/com/javiersc/hubdle/project/kotlin/jvm/features/KotlinJvmMoleculeFeatureTest.kt
@@ -3,10 +3,13 @@ package com.javiersc.hubdle.project.kotlin.jvm.features
 import com.javiersc.gradle.testkit.test.extensions.GradleTestKitTest
 import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledOnOs
+import org.junit.jupiter.api.condition.OS
 
 internal class KotlinJvmMoleculeFeatureTest : GradleTestKitTest() {
 
     @Test
+    @EnabledOnOs(value = [OS.LINUX, OS.MAC])
     fun `molecule feature`() {
         gradleTestKitTest("kotlin/jvm/features/molecule") {
             withArguments("run").build().outputTrimmed.apply {

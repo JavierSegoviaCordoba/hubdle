afd48eea3d2cd5c0122539033f3c8818d74e20d5
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-06-30 00:15:18 +0200

    Fix Compose test

--- hubdle-gradle-plugin/testFunctional/kotlin/com/javiersc/hubdle/project/kotlin/multiplatform/features/KotlinMultiplatformFeaturesComposeTest.kt
+++ hubdle-gradle-plugin/testFunctional/kotlin/com/javiersc/hubdle/project/kotlin/multiplatform/features/KotlinMultiplatformFeaturesComposeTest.kt
@@ -1,8 +1,10 @@
 package com.javiersc.hubdle.project.kotlin.multiplatform.features
 
 import com.javiersc.gradle.testkit.test.extensions.GradleTestKitTest
+import com.javiersc.hubdle.hubdle.gradle.plugin.HubdleGradlePluginProjectData.RootDirAbsolutePath
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import java.io.File
 import kotlin.test.Test
 import org.gradle.testkit.runner.TaskOutcome
 
@@ -11,6 +13,14 @@ internal class KotlinMultiplatformFeaturesComposeTest : GradleTestKitTest() {
     @Test
     fun compose() {
         gradleTestKitTest("kotlin/multiplatform/features/compose") {
+            val hubdleTomlDestination =
+                projectDir.resolve("gradle/hubdle.libs.versions.toml").apply {
+                    parentFile.mkdirs()
+                    createNewFile()
+                }
+            File(RootDirAbsolutePath)
+                .resolve("gradle/hubdle.libs.versions.toml")
+                .copyTo(target = hubdleTomlDestination, overwrite = true)
             gradlew("build").task(":build").shouldNotBeNull().outcome.shouldBe(TaskOutcome.SUCCESS)
         }
     }
--- hubdle-gradle-plugin/testFunctional/resources/kotlin/multiplatform/features/compose/settings.gradle.kts
+++ hubdle-gradle-plugin/testFunctional/resources/kotlin/multiplatform/features/compose/settings.gradle.kts
@@ -11,11 +11,23 @@ plugins {
     id("com.javiersc.hubdle")
 }
 
+hubdleSettings {
+    catalog {
+        isEnabled.set(false)
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
         gradlePluginPortal()
         maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev/")
+    }
+
+    versionCatalogs {
+        register("hubdle") {
+            from(files("gradle/hubdle.libs.versions.toml"))
+        }
     }
 }

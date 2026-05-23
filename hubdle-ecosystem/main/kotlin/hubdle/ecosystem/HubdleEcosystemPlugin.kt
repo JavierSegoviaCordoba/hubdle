2cfd191c7f60ea81f2da1e18ffef34439be75e5a
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2026-05-23 19:57:00 +0200

    Run format

--- hubdle-ecosystem/main/kotlin/hubdle/ecosystem/HubdleEcosystemPlugin.kt
+++ hubdle-ecosystem/main/kotlin/hubdle/ecosystem/HubdleEcosystemPlugin.kt
@@ -8,9 +8,9 @@ import hubdle.ecosystem.feature.documentation.HubdleDocumentationFeature
 import hubdle.ecosystem.feature.documentation.readme.HubdleDocumentationReadmeFeature
 import hubdle.ecosystem.feature.documentation.readme.badges.HubdleDocumentationReadmeBadgesFeature
 import hubdle.ecosystem.feature.kotlin.HubdleKotlinFeature
+import hubdle.ecosystem.feature.projectConfig.HubdleProjectConfigFeature
 import hubdle.ecosystem.feature.versioning.HubdleVersioningFeature
 import hubdle.ecosystem.feature.versioning.semver.HubdleVersioningSemverFeature
-import hubdle.ecosystem.feature.projectConfig.HubdleProjectConfigFeature
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.features.annotations.RegistersProjectFeatures
